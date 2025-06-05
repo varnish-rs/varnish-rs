973fae4356ef153629a332af318a4e74bb3c4b4f
Yuri Astrakhan <yuriastrakhan@gmail.com>
2025-06-05 17:09:59 -0400

    feat: forbid and autofix tab characters (#210)
    tsk tsk tsk...
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- varnish-sys/c_code/wrapper.c
+++ varnish-sys/c_code/wrapper.c
@@ -6,12 +6,12 @@ VRT_VSC_AllocVariadic(struct vsmw_cluster *vc, struct vsc_seg **sg,
     const char *nm, size_t sd,
     const unsigned char *jp, size_t sj, const char *fmt, ...)
 {
-	va_list ap;
-	void *retval;
+    va_list ap;
+    void *retval;
 
-	va_start(ap, fmt);
-	retval = VRT_VSC_Alloc(vc, sg, nm, sd, jp, sj, fmt, ap);
-	va_end(ap);
-	return(retval);
+    va_start(ap, fmt);
+    retval = VRT_VSC_Alloc(vc, sg, nm, sd, jp, sj, fmt, ap);
+    va_end(ap);
+    return(retval);
 }
 #endif
