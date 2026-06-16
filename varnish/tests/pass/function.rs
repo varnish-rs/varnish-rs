58da03a584ab2596485ca2949271807401363355
Thomas Klinger <thomas.klinger@otto.de>
2026-06-16 15:40:15 +0200

    Remove tests for Option<Option<Acl>>
    This is not a supported type for VMODs so the test is removed[1]
    
    [1]:
    https://github.com/varnish-rs/varnish-rs/pull/297#issuecomment-4719273069

--- varnish/tests/pass/function.rs
+++ varnish/tests/pass/function.rs
@@ -61,7 +61,6 @@ mod types {
 
     // Acl
     pub fn opt_acl(_v: Option<Acl>) {}
-    pub fn opt_opt_acl(_v: Option<Option<Acl>>) {}
     pub fn opt_req_acl(#[required] _v: Option<Acl>) {}
     pub fn to_res_acl() -> Result<Option<Acl>, &'static str> {
         Err("")
